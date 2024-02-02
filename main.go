27ac0d533f5e26bc03d8365a746a5a0a3e9c1e81
azzam2912 <azzam.gamers12@gmail.com>
2024-02-02 15:15:54 +0700

    Add Cors Enabler

--- main.go
+++ main.go
@@ -120,6 +120,10 @@ func getAllBlogPostHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(blog_posting)
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+}
+
 func main() {
 	var err error
 	sqlOpeningStatement := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPass, dbName)
